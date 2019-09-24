f301bad7f813230e7483e6741aed0eb6fa968b69
MakotoInomata <inomar1486@gmail.com>
2019-09-24 11:49:41 +0900

    fix

--- frontend/components/layout/SideBar.jsx
+++ frontend/components/layout/SideBar.jsx
@@ -9,7 +9,7 @@ export default () => {
           <a href="/" className="active">検索事例 &gt;</a>
         </li>
         <li>
-          <a href="/mail_templates">メールテンプレート &gt;</a>
+          <a href="/mail_templates" className="active">メールテンプレート &gt;</a>
         </li>
       </SideMenu>
     </div>
